98b469c55a4f5b5e88184af21c2020f9e346ac05
Bennett Dams <dams.bennett@gmail.com>
2021-10-01 01:02:40 +0200

    TEst

--- main.ts
+++ main.ts
@@ -13,7 +13,10 @@ async function takeScreenshot() {
   lastScreenshotPath = path;
 
   console.log("Init bot");
-  const browser = await puppeteer.launch({ userDataDir: "./puppeteer" });
+  const browser = await puppeteer.launch({
+    userDataDir: "./puppeteer",
+    ignoreDefaultArgs: true,
+  });
   const page = await browser.newPage();
   page.setViewport({ width: 1920, height: 1080 });
   await page.goto("https://twitch.tv/montanablack88");
