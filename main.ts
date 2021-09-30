5e376f270f75d56ce77cd7c5596358fa1983b835
Bennett Dams <dams.bennett@gmail.com>
2021-10-01 00:19:36 +0200

    Init puppet folder

--- main.ts
+++ main.ts
@@ -6,11 +6,19 @@ let lastScreenshotPath: string | null = null;
 
 init();
 
-async function takeScreenshot(page: Page) {
+async function takeScreenshot() {
   const now = new Date();
-  const path = `screenshots/trillbot-${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDay()}.png`;
+  const path = `screenshots/trillbot-${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDay()}-${now.getTime()}.png`;
   lastScreenshotPath = path;
+
+  console.log("Init bot");
+  const browser = await puppeteer.launch({ userDataDir: "puppeteer" });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1920, height: 1080 });
+  await page.goto("https://twitch.tv/montanablack88");
+
   await page.screenshot({ path });
+  await browser.close();
 }
 
 function log(text: string) {
@@ -77,20 +85,18 @@ function log(text: string) {
 //   }
 // }
 
-async function initPuppeteer(): Promise<Page> {
-  console.log("Init bot");
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.setViewport({ width: 1920, height: 1080 });
-  await page.goto("https://twitch.tv/montanablack88");
-  // await browser.close();
+// async function initPuppeteer(): Promise<Page> {
+//   console.log("Init bot");
+//   const browser = await puppeteer.launch({ userDataDir: "puppeteer" });
+//   const page = await browser.newPage();
+//   page.setViewport({ width: 1920, height: 1080 });
+//   await page.goto("https://twitch.tv/montanablack88");
+//   await browser.close();
 
-  return page;
-}
+//   return page;
+// }
 
-const page = await initPuppeteer();
-await takeScreenshot(page);
-
+// const page = await initPuppeteer();
 function init() {
   log("INIT");
   lastCheckTs = new Date();
@@ -114,8 +120,10 @@ setInterval(() => {
 //   //   console.log(viewcount);
 // }
 
-function handleRequest() {
+async function handleRequest() {
   log("[REQ]");
+
+  await takeScreenshot();
 
   const lastScreenshot = !lastScreenshotPath
     ? ""
